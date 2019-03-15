a48eca1a59c8b19e70e1855ea1450390af9c618a
Matthias <mreszotnik@gmail.com>
2019-03-15 07:57:20 +0100

    enabling runs in parallel

--- src/library.ts
+++ src/library.ts
@@ -1,20 +1,23 @@
 import 'chromedriver';
+import 'geckodriver';
+import * as assert from 'assert';
 import {WebDriver, Builder, By, ThenableWebDriver, until, WebElement} from 'selenium-webdriver';
 
-    var drive = new Builder().forBrowser('chrome').build();
-
-    async function runDriver(driver:ThenableWebDriver) {
+    async function runDriver(driver:ThenableWebDriver):Promise<void> {
         await driver.manage().timeouts().pageLoadTimeout(3000);
-        // await driver.manage().timeouts().implicitlyWait(1000);
         await driver.navigate().to('https://library-app.firebaseapp.com/');
         await driver.findElement(By.css("#ember14")).sendKeys("ddd@test.com");
         await driver.findElement(By.xpath("//button[contains(text(),'Request')]")).click();
         await driver.wait(until.elementLocated(By.css(".alert")), 3000);
+        await driver.sleep(5000);
         driver.findElement(By.xpath("//button[contains(text(),'Request')]")).getText().then(function(result) {
             console.log(result);
         });
+        // assert(false);
+        
         await driver.close();
-        await driver.quit();
     }
-
-    runDriver(drive);
+['chrome', 'firefox'].forEach((browserName) => {
+    const driver:ThenableWebDriver= new Builder().forBrowser(browserName).build();
+    runDriver(driver);
+})
