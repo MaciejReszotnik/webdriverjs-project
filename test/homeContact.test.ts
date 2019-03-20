31cb667696012ed8c671fc06b76da8d02026c37e
Maciej Reszotnik <Maciej.Reszotnik@ivanti.com>
2019-03-20 13:38:24 +0100

    Implement genuine parallelism, removed hacks

--- test/homeContact.test.ts
+++ test/homeContact.test.ts
@@ -1,47 +1,47 @@
 import 'chromedriver';
-import 'geckodriver';
 import 'mocha';
 import * as assert from 'assert';
-import * as browsers from './data/browserNames.json';
 import * as expectedResult from './data/expectedResults.json';
 import { WebDriverWrapper } from './helpers/webdriverWrapper';
 import { HomePage } from './pageObjects/homePage';
 import { ContactPage } from './pageObjects/contactPage';
 
 import { NavigationComponent } from './pageObjects/pageComponents/navigationComponent';
-import {describe, it, after, before} from 'selenium-webdriver/testing';
+import { describe, it, after, before } from 'selenium-webdriver/testing';
 
 let driver: WebDriverWrapper;
 let homePage: HomePage;
 let contactPage: ContactPage;
 let navigation: NavigationComponent;
 
-['firefox', 'chrome'].reverse().forEach(async (browserName) => {
-    describe(`Home page and Contact page tests - ${browserName} tests`, function() {
-        this.timeout(1000000);
+// Get target browser from environment variable
+const browserName = process.argv[process.argv.length - 1].split('=').pop();
 
-        before(function() {
-            driver = new WebDriverWrapper(browserName);
-            homePage = new HomePage(driver);
-            navigation = new NavigationComponent(driver);
-            homePage.navigateToPage();
-        });
+describe(`Home page and Contact page tests - ${browserName} tests`, function() {
+    this.timeout(1000000);
 
-        after(async function() {
-            await driver.quit();
-        });
+    before(function() {
+        driver = new WebDriverWrapper(browserName);
+        homePage = new HomePage(driver);
+        navigation = new NavigationComponent(driver);
+        homePage.navigateToPage();
+    });
 
-        it('"Kontakt aufnehmen" button on the home page contains correct email address', async function() {
-            homePage.acceptCookies();
-            const emailAttribute = await homePage.getEmailAttribute();
-            assert(emailAttribute.trim().includes(expectedResult.companyEmail));
-        });
+    after(async function() {
+        await driver.quit();
+    });
 
-        it('Contact page contains correct email address', async function() {
-            navigation.clickNavElement('Kontakt');
-            contactPage = new ContactPage(driver);
-            const emailAttribute = await contactPage.getEmailAttribute(expectedResult.companyEmail);
-            assert(emailAttribute.trim().includes(expectedResult.companyEmail));
-        });
+    it('"Kontakt aufnehmen" button on the home page contains correct email address', async function() {
+        homePage.acceptCookies();
+        const emailAttribute = await homePage.getEmailAttribute();
+        assert(emailAttribute.trim().includes(expectedResult.companyEmail));
+    });
+
+    it('Contact page contains correct email address', async function() {
+        navigation.clickNavElement('Kontakt');
+        contactPage = new ContactPage(driver);
+        const emailAttribute = await contactPage.getEmailAttribute(expectedResult.companyEmail);
+        assert(emailAttribute.trim().includes(expectedResult.companyEmail));
     });
 });
+
